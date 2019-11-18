1d64649f765bf4940fa4ae53d7c04c2f083b62bd
skozubenko1 <skozubenko1@eagles.ewu.edu>
2019-11-18 11:42:35 -0800

    assignment 7

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Sorter.Tests
 {
@@ -6,8 +7,151 @@ namespace Sorter.Tests
     public class SortUtilityTests
     {
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void passNullTest()
+        {
+            SortUtility arr = new SortUtility();
+
+            arr.Sorter(null, (left, right) => left > right);
+
+
+        }
+
+        [TestMethod]
         public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
         {
+            SortUtility arr = new SortUtility();
+
+            ComparatorDel ascending = (left, right) => { return left < right; };
+            //ComparatorDel descending = (left, right) => { return left < right; };
+
+           int[] array = createRandom(4);
+
+            arr.Sorter(array, ascending);
+
+
+            Assert.IsTrue(isAscending(array));
+        }
+
+
+        [TestMethod]
+        public void SortUtility_ShouldSortDescending()
+        {
+            SortUtility arr = new SortUtility();
+
+            ComparatorDel descending = (left, right) =>  left > right;
+
+            int[] array = createRandom(4);
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            arr.Sorter(array, descending);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Assert.IsTrue(isDescending(array));
+        }
+
+        [TestMethod]
+        public void TestWithStatement()
+        {
+            SortUtility arr = new SortUtility();
+
+            int[] array = { 1, 2, 3, 4 };
+
+            arr.Sorter(array, (left, right) => {
+                if (left > 2 && right > 2) { return left < right; }
+                return left > right;
+            });
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Assert.IsTrue(isAsAndDes(array));
+
+        }
+
+        static Random random = new Random();
+        public int[] createRandom(int length)
+        {
+            int[] array = new int[length];
+
+            for(int i = 0; i < length; i++)
+            {
+                array[i] = random.Next(0, 100);
+            }
+
+            return array;
+        }
+
+        public bool isDescending(int[] array)
+        {
+            int prev = int.MaxValue;
+            
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(prev < array[i])
+                {
+                    return false;    
+                }
+
+                prev = array[i];
+            }
+
+            return true;
+        }
+
+        public bool isAscending(int[] array)
+        {
+            int prev = int.MinValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (prev > array[i])
+                {
+                    return false;
+                }
+
+                prev = array[i];
+            }
+
+            return true;
+        }
+
+        public bool isAsAndDes(int[] array)
+        {
+            int prev = int.MinValue;
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (prev > array[i])
+                {
+                    return false;
+                }
+
+                prev = array[i];
+            }
+
+            int prev2 = int.MaxValue;
+
+            for (int i = 2; i < array.Length; i++)
+            {
+                if (prev2 < array[i])
+                {
+                    return false;
+                }
+
+                prev2 = array[i];
+            }
+
+            return true;
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -2,8 +2,35 @@
 
 namespace Sorter
 {
+    public delegate bool ComparatorDel(int left, int right);
+
     public class SortUtility
     {
+
+        public void Sorter(int[] array, ComparatorDel comp)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int min = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (comp(array[j], array[min]))
+                    {
+                        min = j;
+                    }
+                }
+
+                int temp = array[min];
+                array[min] = array[i];
+                array[i] = temp;
+            }
+        }
         // Sort method should be implemented here
         // It should accept an int[] and a delegate you define that performs the actual comparison
     }
